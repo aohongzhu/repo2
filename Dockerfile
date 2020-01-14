d83e5db681a0b0b2784ff2b59493f838fae6d9c6
敖宏柱 <aohongzhu@bailun.org>
2020-01-14 16:20:23 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM openjdk:8-jre-alpine
+MAINTAINER Aohongzhu <ahz_393123108@163.com>
+ENV LANG     zh_CN.UTF-8
+ENV LANGUAGE zh_CN.UTF-8
+ENV LC_ALL   zh_CN.UTF-8
+#设置时区
+RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
+    echo "Asia/Shanghai" > /etc/timezone
+
+ADD target/springboot_demo-*.jar /test-service.jar
+ENTRYPOINT ["java","-jar", "test-service.jar"]
+EXPOSE 8081
---
+++ src/main/java/com/example/springboot_demo/SpringBootTestController.java
@@ -0,0 +1,20 @@
+package com.example.springboot_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author Aohongzhu
+ * @date 2020年01月14日
+ */
+@RestController
+public class SpringBootTestController {
+    @GetMapping(value = "/hello")
+    public String welcome(){
+        return "welcome to springboot world!!!";
+    }
+}
